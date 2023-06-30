f8ae0243f9e2e4c44226ecddd3d6bb1f77855d75
lokesh mutchukota <lokeshmutchukota@gmail.com>
2023-06-30 17:35:05 +0530

    ClientTicket API Changes is done

---
+++ src/main/java/com/unicorn/indsaccrm/clientticket/ClientTicketController.java
@@ -0,0 +1,31 @@
+package com.unicorn.indsaccrm.clientticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/clientticket")
+public class ClientTicketController {
+
+    @Autowired
+    ClientTicketService clientTicketService;
+
+
+
+    @PostMapping
+    public ResponseEntity<?> ClientTicket(@RequestBody ClientTicket values) throws Exception {
+        return ResponseEntity.ok(clientTicketService.saveClientTicket(values));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllClientTickets() throws Exception {
+        return ResponseEntity.ok(clientTicketService.getAllClientTickets());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getByClentTcketId(@PathVariable UUID id) throws Exception {
+        return ResponseEntity.ok(clientTicketService.getByAdminDefaultValuesId(id));
+    }
+}
---
+++ src/main/java/com/unicorn/indsaccrm/clientticket/ClientTicketRepository.java
@@ -0,0 +1,8 @@
+package com.unicorn.indsaccrm.clientticket;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClientTicketRepository extends JpaRepository<ClientTicket, UUID> {
+}
